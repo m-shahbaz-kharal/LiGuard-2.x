4f2a72dba69feae2a8ba4cac042f57d967d93f1d
m-shahbaz-kharal <m.shahbaz.kharal@outlook.com>
2024-02-27 20:15:11 -0500

    decoupled io from visualizer.

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from pcd.file_io import FileIO as PCD_File_IO
 from pcd.sensor_io import SensorIO as PCD_Sensor_IO
 from pcd.viz import PointCloudVisualizer
 
-from rgb.file_io import FileIO as RGB_File_IO
-from rgb.sensor_io import SensorIO as RGB_Sensor_IO
-from rgb.viz import ImageVisualizer
+from img.file_io import FileIO as IMG_File_IO
+from img.sensor_io import SensorIO as IMG_Sensor_IO
+from img.viz import ImageVisualizer
 
 import keyboard, threading, time
 
@@ -26,54 +26,77 @@ class LiGuard:
         self.pcd_io = None
         self.pcd_visualizer = None
         
-        self.rgb_io = None
-        self.rgb_visualizer = None
+        self.img_io = None
+        self.img_visualizer = None
         
-        self.is_running = threading.Event()
+        self.lock = threading.Lock()
+        self.is_running = False # if the app is running
+        self.is_playing = False # if the frames are playing
+        self.frame_index = 0
+        
+        keyboard.hook(self.handle_key_event)
         self.app.run()
         
+    def handle_key_event(self, event:keyboard.KeyboardEvent):
+        if event.event_type == keyboard.KEY_DOWN:
+            with self.lock:
+                if event.name == 'right':
+                    self.is_playing = False
+                    if self.frame_index < len(self.pcd_io) - 1: self.frame_index += 1
+                elif event.name == 'left':
+                    self.is_playing = False
+                    if self.frame_index > 0: self.frame_index -= 1
+                elif event.name == 'space':
+                    self.is_playing = not self.is_playing
+        
     def reset(self, cfg):
-        self.is_running.clear()
-        keyboard.unhook_all()
+        with self.lock: self.is_running = False
         
         if self.pcd_io != None: self.pcd_io.close()
         if cfg.sensors.lidar.enabled: self.pcd_io = PCD_Sensor_IO(cfg)
         else: self.pcd_io = PCD_File_IO(cfg)
         
-        if self.rgb_io != None: self.rgb_io.close()
-        if cfg.sensors.camera.enabled: self.rgb_io = RGB_Sensor_IO(cfg)
-        else: self.rgb_io = self.rgb_io = RGB_File_IO(cfg)
+        if self.img_io != None: self.img_io.close()
+        if cfg.sensors.camera.enabled: self.img_io = IMG_Sensor_IO(cfg)
+        else: self.img_io = self.img_io = IMG_File_IO(cfg)
              
-        if self.pcd_visualizer != None: self.pcd_visualizer.reset(cfg, self.pcd_io)
-        else: self.pcd_visualizer = PointCloudVisualizer(self.app, cfg, self.pcd_io)
-        keyboard.hook(self.pcd_visualizer.key_handler)
+        if self.pcd_visualizer != None: self.pcd_visualizer.reset(cfg)
+        else: self.pcd_visualizer = PointCloudVisualizer(self.app, cfg)
                     
-        if self.rgb_visualizer != None: self.rgb_visualizer.reset(cfg, self.rgb_io)
-        else: self.rgb_visualizer = ImageVisualizer(self.app, cfg, self.rgb_io)
-        keyboard.hook(self.rgb_visualizer.key_handler)
-        
-        pcd_viz_begin_fn, pcd_viz_loop_fn, pcd_viz_end_fn =  self.pcd_visualizer.get_main_functions()
-        rgb_viz_begin_fn, rgb_viz_loop_fn, rgb_viz_end_fn =  self.rgb_visualizer.get_main_functions()
+        if self.img_visualizer != None: self.img_visualizer.reset(cfg)
+        else: self.img_visualizer = ImageVisualizer(self.app, cfg)
         
         # main loop
-        pcd_viz_begin_fn()
-        rgb_viz_begin_fn()
-        self.is_running.set()
-        while self.is_running.is_set():
-            pcd_viz_loop_fn()
-            rgb_viz_loop_fn()
+        with self.lock: self.is_running = True
+        while True:
+            with self.lock:
+                if not self.is_running: break
+                elif self.is_playing and self.frame_index < len(self.pcd_io) - 1: self.frame_index += 1
+            
+            pcd_np = self.pcd_io[self.frame_index]
+            img_np = self.img_io[self.frame_index]
+            
+            self.pcd_visualizer.update_pcd(pcd_np)
+            self.img_visualizer.update_img(img_np)
+            
+            self.pcd_visualizer.redraw()
+            self.img_visualizer.redraw() 
+            
             time.sleep(cfg.debug.asyncio_sleep)
-        pcd_viz_end_fn()
-        rgb_viz_end_fn()
             
     def quit(self, cfg):
         keyboard.unhook_all()
-        if self.pcd_io != None: self.pcd_io.close()
-        if self.rgb_io != None: self.rgb_io.close()
-        self.is_running.clear()
+        with self.lock: self.is_running = False
+        
+        self.pcd_io.close()
+        self.img_io.close()
+        
+        self.pcd_visualizer.quit()
+        self.img_visualizer.quit()
+        
         self.app.quit()
         
 def main():
-    liguard = LiGuard()
+    LiGuard()
     
 main()
