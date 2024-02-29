b1fcafa0d9688991d1fbf42e5d6ed64377e881de
m-shahbaz-kharal <m.shahbaz.kharal@outlook.com>
2024-02-28 21:04:00 -0500

    moved pcd/proc/utils module to pcd/utils.

---
+++ pcd/utils.py
@@ -0,0 +1,12 @@
+import open3d as o3d
+import numpy as np
+import cv2
+
+def create_pcd(points: np.ndarray) -> o3d.geometry.PointCloud:
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points[:, 0:3])
+    return pcd
+
+def update_pcd(pcd: o3d.geometry.PointCloud, points: np.ndarray) -> o3d.geometry.PointCloud:
+    pcd.points = o3d.utility.Vector3dVector(points[:, 0:3])
+    return pcd
