6fa711c9a8478452fb06a3e4aada76854959962f
m-shahbaz-kharal <m.shahbaz.kharal@outlook.com>
2024-03-05 12:12:09 -0500

    Update color codes for object types in handler_sustechpoints.py

--- lbl/handler_sustechpoints.py
+++ lbl/handler_sustechpoints.py
@@ -4,15 +4,20 @@ import open3d as o3d
 import json
 
 colors = {
-    'Car': [0, 1, 0],
-    'Van': [0, 1, 0],
-    'Truck': [0, 1, 0],
-    'Pedestrian': [1, 0, 0],
-    'Person_sitting': [1, 0, 0],
-    'Cyclist': [0, 0, 1],
-    'Tram': [1, 1, 0],
-    'Misc': [1, 1, 0],
-    'DontCare': [1, 1, 1]
+    "Car":            (0  ,255,0  ),#'#00ff00',
+    "Van":            (0  ,255,0  ),#'#00ff00',
+    "Bus":            (0  ,255,255),#'#ffff00', 
+    "Pedestrian":     (0  ,0  ,255),#'#ff0000',
+    "Rider":          (0  ,136,255),#'#ff8800',
+    "Cyclist":        (0  ,136,255),#'#ff8800',
+    "Bicycle":        (0  ,255,136),#'#88ff00',
+    "BicycleGroup":   (0  ,255,136),#'#88ff00',
+    "Motor":          (0  ,176,176),#'#aaaa00',
+    "Truck":          (255,255,0  ),#'#00ffff',
+    "Tram":           (255,255,0  ),#'#00ffff',
+    "Animal":         (255,176,0  ),#'#00aaff',
+    "Misc":           (136,136,0  ),#'#008888',
+    "Unknown":        (136,136,0  ),#'#008888',
 }
 
 def Handler(label_path: str, calib_path: str):
@@ -22,7 +27,7 @@ def Handler(label_path: str, calib_path: str):
     if os.path.exists(label_path) == False: return output
     with open(label_path, 'r') as f: lbls = json.load(f)
     for item in lbls:
-        # annotator = item['annotator']
+        annotator = item['annotator'] if 'annotator' in item else 'Unknown'
         obj_id = int(item['obj_id'])
         obj_type = item['obj_type']
         psr = item['psr']
@@ -31,7 +36,7 @@ def Handler(label_path: str, calib_path: str):
         psr_scale_xyz = [float(psr['scale']['x']), float(psr['scale']['y']), float(psr['scale']['z'])]
         
         label = dict()
-        # label['annotator'] = annotator
+        label['annotator'] = annotator
         label['id'] = obj_id
         label['type'] = obj_type
         label['psr'] = psr
@@ -40,14 +45,14 @@ def Handler(label_path: str, calib_path: str):
         # lidar_xyz_center[2] += psr_scale_xyz[2] / 2.0
         lidar_wlh_extent = np.array(psr_scale_xyz, dtype=np.float32)
         lidar_around_z_rotation_matrix = o3d.geometry.OrientedBoundingBox.get_rotation_matrix_from_xyz(psr_rotation_xyz)
-        lidar_bbox_color = colors[obj_type]
+        lidar_bbox_color = [i / 255.0 for i in colors[obj_type]]
         
         label['lidar_bbox'] = {'xyz_center': lidar_xyz_center, 'wlh_extent': lidar_wlh_extent, 'around_z_rotation_matrix': lidar_around_z_rotation_matrix, 'rgb_bbox_color': lidar_bbox_color}
         
         camera_xyz_center = np.array(psr_position_xyz, dtype=np.float32)
         camera_wlh_extent = np.array(psr_scale_xyz, dtype=np.float32)
         camera_around_y_rotation_matrix = o3d.geometry.OrientedBoundingBox.get_rotation_matrix_from_xyz(psr_rotation_xyz)
-        camera_bbox_color = [i * 255 for i in colors[obj_type]]
+        camera_bbox_color = colors[obj_type]
         
         label['camera_bbox'] = {'xyz_center': camera_xyz_center, 'wlh_extent': camera_wlh_extent, 'around_y_rotation_matrix': camera_around_y_rotation_matrix, 'rgb_bbox_color': camera_bbox_color}
         
