013586f2cfcbcfe83bc163dcc448137f905acabb
m-shahbaz-kharal <m.shahbaz.kharal@outlook.com>
2024-03-14 21:00:20 -0400

    added fix_number_of_points_in_pcd function to utils.py

--- pcd/utils.py
+++ pcd/utils.py
@@ -9,3 +9,10 @@ def create_pcd(points: np.ndarray) -> o3d.geometry.PointCloud:
 def update_pcd(pcd: o3d.geometry.PointCloud, points: np.ndarray) -> o3d.geometry.PointCloud:
     pcd.points = o3d.utility.Vector3dVector(points[:, 0:3])
     return pcd
+
+def fix_number_of_points_in_pcd(point_cloud: np.ndarray, number_of_points: int) -> np.ndarray:
+    # pad if less points
+    if point_cloud.shape[0] < number_of_points: point_cloud = np.pad(point_cloud, ((0, number_of_points-point_cloud.shape[0]), (0,0)), mode='constant', constant_values=0)
+    # crop if more points
+    elif point_cloud.shape[0] > number_of_points: point_cloud = point_cloud[:number_of_points]
+    return point_cloud
