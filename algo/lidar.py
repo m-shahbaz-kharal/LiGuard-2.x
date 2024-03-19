37c3c351e495d7a9b6b6df508b54f720dec722c2
Muhammad Shahbaz <m.shahbaz.kharal@outlook.com>
2024-03-19 14:41:49 -0400

    added Spatio-Temporal Density Filter (STDF) in background filters.

--- algo/lidar.py
+++ algo/lidar.py
@@ -89,3 +89,56 @@ def BGFilterDHistDPP(data_dict: dict, cfg_dict: dict):
     if filter_name in data_dict:
         data_dict['current_point_cloud_numpy'] = fix_number_of_points_in_pcd(data_dict['current_point_cloud_numpy'], number_of_points_per_frame)
         data_dict['current_point_cloud_numpy'] = data_dict['current_point_cloud_numpy'][data_dict[filter_name](data_dict['current_point_cloud_numpy'], background_density_threshold)]
+
+def BGFilterSTDF(data_dict: dict, cfg_dict: dict):
+    algo_name = 'BGFilterSTDF'
+    query_frames_key = f'{algo_name}_query_frames'
+    skip_frames_key = f'{algo_name}_skip_frames'
+    filter_name = f'{algo_name}_filter'
+    
+    number_of_frame_gather_iters = cfg_dict['proc']['lidar']['BGFilterSTDF']['number_of_frame_gather_iters']
+    number_of_frames_in_each_gather_iter = cfg_dict['proc']['lidar']['BGFilterSTDF']['number_of_frames_in_each_gather_iter']
+    number_of_skip_frames_after_each_iter = cfg_dict['proc']['lidar']['BGFilterSTDF']['number_of_skip_frames_after_each_iter']
+    number_of_points_per_frame = cfg_dict['proc']['lidar']['BGFilterSTDF']['number_of_points_per_frame']
+    lidar_range_in_unit_length = cfg_dict['proc']['lidar']['BGFilterSTDF']['lidar_range_in_unit_length']
+    bins_per_unit_length = cfg_dict['proc']['lidar']['BGFilterSTDF']['bins_per_unit_length']
+    background_density_threshold = cfg_dict['proc']['lidar']['BGFilterSTDF']['background_density_threshold']
+    
+    if filter_name not in data_dict:
+        data_dict[f'{algo_name}_number_of_frame_gather_iters'] = number_of_frame_gather_iters
+        data_dict[f'{algo_name}_number_of_frames_in_each_gather_iter'] = number_of_frames_in_each_gather_iter
+        data_dict[f'{algo_name}_number_of_skip_frames_after_each_iter'] = number_of_skip_frames_after_each_iter
+        data_dict[f'{algo_name}_number_of_points_per_frame'] = number_of_points_per_frame
+        data_dict[f'{algo_name}_lidar_range_in_unit_length'] = lidar_range_in_unit_length
+        data_dict[f'{algo_name}_bins_per_unit_length'] = bins_per_unit_length
+        
+        for i in range(number_of_frame_gather_iters):
+            gathering_done = gather_point_clouds(data_dict, cfg_dict, f'{query_frames_key}_{i}', number_of_frames_in_each_gather_iter)
+            if not gathering_done: return
+            skipping_done = skip_frames(data_dict, cfg_dict, f'{skip_frames_key}_{i}', number_of_skip_frames_after_each_iter)
+            if not skipping_done: return
+        
+        gather_keys = [f'{query_frames_key}_{i}' for i in range(number_of_frame_gather_iters)]
+        combine_gathers(data_dict, cfg_dict, query_frames_key, gather_keys)
+        assert len(data_dict[query_frames_key]) == number_of_frame_gather_iters * number_of_frames_in_each_gather_iter
+        
+        from algo.background_filters.STDF import STDF
+        data_dict[query_frames_key] = [fix_number_of_points_in_pcd(frame, number_of_points_per_frame) for frame in data_dict[query_frames_key]]
+        data_dict[filter_name] = STDF(data_dict[query_frames_key], lidar_range_in_unit_length, bins_per_unit_length)
+    else:
+        condition = False
+        condition = condition or data_dict[f'{algo_name}_number_of_frame_gather_iters'] != number_of_frame_gather_iters
+        condition = condition or data_dict[f'{algo_name}_number_of_frames_in_each_gather_iter'] != number_of_frames_in_each_gather_iter
+        condition = condition or data_dict[f'{algo_name}_number_of_skip_frames_after_each_iter'] != number_of_skip_frames_after_each_iter
+        condition = condition or data_dict[f'{algo_name}_number_of_points_per_frame'] != number_of_points_per_frame
+        condition = condition or data_dict[f'{algo_name}_lidar_range_in_unit_length'] != lidar_range_in_unit_length
+        condition = condition or data_dict[f'{algo_name}_bins_per_unit_length'] != bins_per_unit_length
+        
+        if condition:
+            keys_to_remove = [key for key in data_dict.keys() if key.startswith(algo_name)]
+            for key in keys_to_remove: data_dict.pop(key)
+            return
+    
+    if filter_name in data_dict:
+        data_dict['current_point_cloud_numpy'] = fix_number_of_points_in_pcd(data_dict['current_point_cloud_numpy'], number_of_points_per_frame)
+        data_dict['current_point_cloud_numpy'] = data_dict['current_point_cloud_numpy'][data_dict[filter_name](data_dict['current_point_cloud_numpy'], background_density_threshold)]
