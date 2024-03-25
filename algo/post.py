3f32e2a17dad57dbbe42038379ffa8d52d2c49b4
m-shahbaz-kharal <m.shahbaz.kharal@outlook.com>
2024-03-25 19:47:04 -0400

    removed per frame sub-directories.

--- algo/post.py
+++ algo/post.py
@@ -14,6 +14,10 @@ def create_per_object_pcdet_dataset(data_dict: dict, cfg_dict: dict):
     
     output_path = os.path.join(cfg_dict['data']['path'], 'output', 'post', 'per_object_pcdet_dataset')
     os.makedirs(output_path, exist_ok=True)
+    pcd_output_dir = os.path.join(output_path, 'point_cloud')
+    os.makedirs(pcd_output_dir, exist_ok=True)
+    lbl_output_dir = os.path.join(output_path, 'label')
+    os.makedirs(lbl_output_dir, exist_ok=True)
     
     for idx, label_dict in enumerate(current_label_list):
         bbox_center = label_dict['lidar_bbox']['lidar_xyz_center'].copy()
@@ -29,15 +33,10 @@ def create_per_object_pcdet_dataset(data_dict: dict, cfg_dict: dict):
         bbox_center -= point_cloud_mean
         object_point_cloud[:, :3] -= point_cloud_mean
         
-        per_frame_output_path = os.path.join(output_path, os.path.basename(current_label_path).replace('.txt', ''))
-        os.makedirs(per_frame_output_path, exist_ok=True)
-        os.makedirs(os.path.join(per_frame_output_path, 'point_cloud'), exist_ok=True)
-        os.makedirs(os.path.join(per_frame_output_path, 'label'), exist_ok=True)
-        
-        npy_path = os.path.join(per_frame_output_path, 'point_cloud', f'{idx}.npy')
+        npy_path = os.path.join(pcd_output_dir, os.path.basename(current_label_path).replace('.txt', f'{str(idx).zfill(4)}.npy'))
         np.save(npy_path, object_point_cloud)
         
-        lbl_path = os.path.join(per_frame_output_path, 'label', f'{idx}.txt')
+        lbl_path = os.path.join(lbl_output_dir, os.path.basename(current_label_path).replace('.txt', f'{str(idx).zfill(4)}.txt'))
         with open(lbl_path, 'w') as f:
             lbl_str = ''
             lbl_str += str(bbox_center[0]) + ' ' + str(bbox_center[1]) + ' ' + str(bbox_center[2]) + ' '
