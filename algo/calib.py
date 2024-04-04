0bee4f620439ec77c043d225270889d65b381184
Muhammad Shahbaz <m.shahbaz.kharal@outlook.com>
2024-04-04 12:36:23 -0400

    added basic template to dummy function

--- algo/calib.py
+++ algo/calib.py
@@ -1,2 +1,16 @@
+from gui.logger_gui import Logger
+
 def dummy(data_dict: dict, cfg_dict: dict):
-    print("A calib dummy function called.")
+    logger:Logger = data_dict['logger']
+
+    if 'current_calib_path' not in data_dict:
+        logger.log('[algo->calib.py->dummy]: current_calib_path not found in data_dict', Logger.DEBUG)
+    
+    if 'current_calib_data' not in data_dict:
+        logger.log('[algo->calib.py->dummy]: current_calib_data not found in data_dict', Logger.ERROR)
+        return
+    
+    current_calib_path = data_dict['current_calib_path']
+    current_calib_data = data_dict['current_calib_data']
+
+    # do ops on current_calib_data as per requirement
