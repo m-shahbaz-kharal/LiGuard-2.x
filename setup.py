79486975b5b4eb9cfba6e9648c8d9cb359fd4a12
Muhammad Shahbaz <m.shahbaz.kharal@outlook.com>
2025-03-28 21:39:27 -0400

    fix: bump version to 2.1.4 and update open3d dependency to 0.19.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import glob
 
 setup(
     name="LiGuard",
-    version="2.1.3",
+    version="2.1.4",
     author="Muhammad Shahbaz",
     author_email="m.shahbaz.kharal@outlook.com",
     description=(
@@ -39,7 +39,7 @@ setup(
     install_requires=[
         "matplotlib",
         "numpy<2.0.0",
-        "open3d==0.18.0",
+        "open3d==0.19.0",
         "opencv-python",
         "pynput",
         "pillow",
