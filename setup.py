9f3b977f0f5f94813cd8b64c476db87c87ba4f88
Muhammad Shahbaz <m.shahbaz.kharal@outlook.com>
2025-04-17 21:10:12 -0400

    chore: update version to 2.1.5.post1 for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import glob
 
 setup(
     name="LiGuard",
-    version="2.1.5",
+    version="2.1.5.post1",
     author="Muhammad Shahbaz",
     author_email="m.shahbaz.kharal@outlook.com",
     description=(
